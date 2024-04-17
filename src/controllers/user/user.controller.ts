019328e8f6bd074be6a638c6b22b6283f5eee7fa
ekirkizheva <kirkizhevaelena@gmail.com>
2024-04-17 22:54:40 +1000

    add comment

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -5,6 +5,12 @@ import { UserService } from 'src/services/user/user.service';
 export class UserController {
     constructor(private userService: UserService){}
 
+    /**
+     * This endpoint is used to authenticate users. 
+     * 
+     * @param signInDto - expects json payload with username and password. 
+     * @returns 
+     */
     @Post('signin')
     signIn(@Body() signInDto: Record<string, string>) {
         return this.userService.signIn(signInDto.username, signInDto.password);
