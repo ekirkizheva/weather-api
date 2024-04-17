e36f183de7e825de767ed29318855a549f24ea0c
ekirkizheva <kirkizhevaelena@gmail.com>
2024-04-17 22:03:50 +1000

    Fix naming

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-@Schema({collection: 'User'})
+@Schema({collection: 'Users'})
 export class UserRecord {
    @Prop()
    username: string;
