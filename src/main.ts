3b7a75fce3d44539ac407769301e6698faa8116c
ekirkizheva <kirkizhevaelena@gmail.com>
2024-04-18 22:59:37 +1000

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: 'https://www.test-cors.org',
+  });
   await app.listen(3000);
 }
 bootstrap();
